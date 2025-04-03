01106ff7306762e9a8421aaea8ce3bb03c176d45
KAWABATA Norio <kawabata@neoscorp.jp>
2025-04-03 12:02:07 +0900

    lambda skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { handle } from 'hono/aws-lambda'
 import { logger } from 'hono/logger'
 import routes from './routes'
 
@@ -6,4 +7,4 @@ const app = new Hono()
 app.use(logger())
 app.route('/', routes)
 
-export default app
+export const handler = handle(app)
